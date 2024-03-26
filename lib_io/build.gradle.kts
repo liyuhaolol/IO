0d09b19e421aa96a83bb84a7e66b14d731f0f696
liyuhao <liyuhaoid@sina.com>
2024-03-26 20:37:38 +0800

    更新1.1.3

---
+++ lib_io/build.gradle.kts
@@ -0,0 +1,112 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+import net.thebugmc.gradle.sonatypepublisher.PublishingType.USER_MANAGED
+
+plugins {
+    id("com.android.library")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
+}
+
+val versionName = "1.1.3"
+
+android {
+    namespace = "spa.lyh.cn.lib_utils"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 19
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled  = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility =  JavaVersion.VERSION_1_8
+    }
+
+}
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+var ossrhUsername = ""//sonatype用户名
+var ossrhPassword = "" //sonatype密码
+
+val localProperties = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signingKeyId")
+    signingPassword = properties.getProperty("signingPassword")
+    secretKeyRingFile = properties.getProperty("secretKeyRingFile")
+    ossrhUsername = properties.getProperty("ossrhUsername")
+    ossrhPassword = properties.getProperty("ossrhPassword")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+
+centralPortal {
+    username = ossrhUsername
+    password = ossrhPassword
+    name = "IO"
+    group = "io.github.liyuhaolol"
+    version = versionName
+    pom {
+        //packaging = "aar"
+        name = "IO"
+        description = "Android IO Utils"
+        url = "https://github.com/liyuhaolol/IO"
+        licenses {
+            license {
+                name = "The MIT License"
+                url = "https://github.com/liyuhaolol/IO/blob/master/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "liyuhao"
+                name = "liyuhao"
+                email = "liyuhaoid@sina.com"
+            }
+        }
+        scm {
+            connection = "scm:git@github.com/liyuhaolol/IO.git"
+            developerConnection = "scm:git@github.com/liyuhaolol/IO.git"
+            url = "https://github.com/liyuhaolol/IO"
+        }
+
+    }
+    publishingType = USER_MANAGED
+    javadocJarTask = tasks.create<Jar>("javadocEmptyJar") {
+        archiveClassifier = "javadoc"
+    }
+
+}
+
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
+
