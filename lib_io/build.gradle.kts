829420fad70677c2f822ba665859fef50e4e9d3f
liyuhao <liyuhaoid@sina.com>
2024-04-02 14:17:01 +0800

    发布1.1.4

--- lib_io/build.gradle.kts
+++ lib_io/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
 }
 
-val versionName = "1.1.3"
+val versionName = "1.1.4"
 
 android {
     namespace = "spa.lyh.cn.utils_io"
